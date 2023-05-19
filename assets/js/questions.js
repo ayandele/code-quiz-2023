b178dfb98e9e02b0c22203efede5add52a8195ae
ayandele <adele.ayansola@yahoo.com>
2023-05-19 12:41:35 -0400

    revised code

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+// Define an array of quiz questions and answers
+const quizQuestions = [
+    {
+    question: "Question 1",
+    answers: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ],
+    correctAnswer: 1
+    },
+    {
+    question: "Question 2",
+    answers: [
+        "Answer 1",
+        "Answer 2",
+        "Answer 3",
+        "Answer 4"
+    ],
+    correctAnswer: 2
+    },
+    // Add more questions as needed
+];
+
+  // Export the quiz questions array
+export default quizQuestions;
---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Get DOM elements
+const startQuizBox = document.getElementById("startQuizBox");
+const quizQuestionsContainer = document.getElementById("quizQuestionscontainer");
+const endQuizContainer = document.getElementById("endtheQuizContainer");
+
+// Add event listeners
+document.getElementById("startQuiz").addEventListener("click", startQuiz);
+document.getElementById("submitYourscorebutton").addEventListener("click", submitScore);
+document.getElementById("restartThequizbutton").addEventListener("click", restartQuiz);
+
+// Start the quiz
+function startQuiz() {
+  // Add your code to start the quiz
+}
+
+// Submit the score
+function submitScore() {
+  // Add your code to submit the score
+}
+
+// Restart the quiz
+function restartQuiz() {
+  // Add your code to restart the quiz
+}
